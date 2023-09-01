0f2ba2a41456b6bd4150d6a4ead1c0c0a9620d35
Arina <arinacypcova@gmail.com>
2023-09-01 20:01:17 +0700

    Added Task

---
+++ Task/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] ResultArray(string[] firstArray)
+{
+    int newSize = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3) newSize++;
+    }
+    string[] newArray = new string[newSize];
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            newArray[count] = firstArray[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+string[] InputArray(int size)
+{
+    string[] stringArray = new string[size];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = GetInput("Введите количество элементов: ");
+string[] firstArray = InputArray(size);
+string[] secondArray = ResultArray(firstArray);
+
+Console.WriteLine($"[{String.Join(", ", (firstArray))}] -> [{String.Join(", ", (secondArray))}]");
